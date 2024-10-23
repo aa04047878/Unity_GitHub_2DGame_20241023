4684171b29f14410a340847e17c5d14898ac9766
aa04047878 <aa04047878@gmail.com>
2024-10-23 16:37:04 +0800

    Update 建立場景控制器

---
+++ Assets/SceneControl.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneControl : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        //Â÷¶}¹CÀ¸
+        Application.Quit();
+    }
+
+
+    
+}
